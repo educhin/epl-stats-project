bbffa80d8143022aeb6edf26ba0f92222c4d0435
Chinedu Arinze <c.s.arinze@gmail.com>
2019-05-22 01:51:29 +0000

    first commit

---
+++ lib/epl/stats/project.rb
@@ -0,0 +1,10 @@
+require "epl/stats/project/version"
+
+module Epl
+  module Stats
+    module Project
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/epl/stats/project/version.rb
@@ -0,0 +1,7 @@
+module Epl
+  module Stats
+    module Project
+      VERSION = "0.1.0"
+    end
+  end
+end
